fb019c42496d0566011d78e83755167e3c0b1f13
Richard Selneck <blakpilar@gmail.com>
2015-11-01 10:47:54 -0500

    Format code, remove potential memory leak

--- Source/MyBoundingObjectClass.cpp
+++ Source/MyBoundingObjectClass.cpp
@@ -3,117 +3,116 @@
 
 MyBoundingObjectClass::MyBoundingObjectClass(const std::vector<vector3>& a_lVectorList)
 {
-	m_pBoundingBox = new MyBoundingBoxClass(a_lVectorList);
+    m_pBoundingBox = std::make_shared<MyBoundingBoxClass>(a_lVectorList);
 
-	m_pReorientedBoundingBox = new MyBoundingBoxClass(a_lVectorList);
+    m_pReorientedBoundingBox = std::make_shared<MyBoundingBoxClass>( a_lVectorList );
 
-	m_pBoundingSphere = new MyBoundingSphereClass(a_lVectorList);
+    m_pBoundingSphere = std::make_shared<MyBoundingSphereClass>( a_lVectorList );
 }
 
 MyBoundingObjectClass::MyBoundingObjectClass(MyBoundingObjectClass const& other)
 {
-	m_m4ToWorld = other.m_m4ToWorld;
+    m_m4ToWorld = other.m_m4ToWorld;
 
-	m_v3Center = other.m_v3Center;
-	m_v3Min = other.m_v3Min;
-	m_v3Max = other.m_v3Max;
+    m_v3Center = other.m_v3Center;
+    m_v3Min = other.m_v3Min;
+    m_v3Max = other.m_v3Max;
 
-	m_bVisibility = other.m_bVisibility;
-	m_v3Color = other.m_v3Color;
+    m_bVisibility = other.m_bVisibility;
+    m_v3Color = other.m_v3Color;
 
-	m_pBoundingBox = new MyBoundingBoxClass(*other.m_pBoundingBox);
-	m_pReorientedBoundingBox = new MyBoundingBoxClass(*other.m_pReorientedBoundingBox);
-	m_pBoundingSphere = new MyBoundingSphereClass(*other.m_pBoundingSphere);
+    m_pBoundingBox = other.m_pBoundingBox;
+    m_pReorientedBoundingBox = other.m_pReorientedBoundingBox;
+    m_pBoundingSphere = other.m_pBoundingSphere;
 }
 
 MyBoundingObjectClass& MyBoundingObjectClass::operator=(MyBoundingObjectClass const& other)
 {
-	m_m4ToWorld = other.m_m4ToWorld;
+    m_m4ToWorld = other.m_m4ToWorld;
 
-	m_v3Center = other.m_v3Center;
-	m_v3Min = other.m_v3Min;
-	m_v3Max = other.m_v3Max;
+    m_v3Center = other.m_v3Center;
+    m_v3Min = other.m_v3Min;
+    m_v3Max = other.m_v3Max;
 
-	m_bVisibility = other.m_bVisibility;
-	m_v3Color = other.m_v3Color;
+    m_bVisibility = other.m_bVisibility;
+    m_v3Color = other.m_v3Color;
 
-	*m_pBoundingBox = *other.m_pBoundingBox;
-	*m_pReorientedBoundingBox = *other.m_pReorientedBoundingBox;
-	*m_pBoundingSphere = *other.m_pBoundingSphere;
+    *m_pBoundingBox = *other.m_pBoundingBox;
+    *m_pReorientedBoundingBox = *other.m_pReorientedBoundingBox;
+    *m_pBoundingSphere = *other.m_pBoundingSphere;
 
-	return *this;
+    return *this;
 }
 
 MyBoundingObjectClass::~MyBoundingObjectClass()
 {
-	delete m_pBoundingBox;
-	delete m_pReorientedBoundingBox;
-	delete m_pBoundingSphere;
 }
 
 bool MyBoundingObjectClass::IsColliding(MyBoundingObjectClass* const a_pOther) const
 {
-	// Checks if the bounding sphere is colliding
-	if (!m_pBoundingSphere->IsColliding(a_pOther->m_pBoundingSphere))
-	{
-		return false;
-	}
+    // Checks if the bounding sphere is colliding
+    if (!m_pBoundingSphere->IsColliding(a_pOther->m_pBoundingSphere.get()))
+    {
+        return false;
+    }
 
-	// Checks if oriented bounding box is colliding
-	return (m_pReorientedBoundingBox->IsColliding(a_pOther->m_pReorientedBoundingBox));
+    // Checks if oriented bounding box is colliding
+    return (m_pReorientedBoundingBox->IsColliding(a_pOther->m_pReorientedBoundingBox.get()));
 
-	// In the future, we would add Oriented Bounding Box collision
+    // In the future, we would add Oriented Bounding Box collision
 }
 
 void MyBoundingObjectClass::Draw(MeshManagerSingleton* a_pMeshMngr)
 {
-	a_pMeshMngr->AddCubeToQueue(glm::translate(m_pBoundingBox->GetCenterGlobal()) * glm::extractMatrixRotation(m_m4ToWorld) * glm::scale(m_pBoundingBox->GetHalfWidth() * 2.0f), m_v3Color, WIRE);
-	a_pMeshMngr->AddCubeToQueue(glm::translate(m_pReorientedBoundingBox->GetCenterGlobal()) * glm::scale(m_pReorientedBoundingBox->GetHalfWidth() * 2.0f), m_v3Color, WIRE);
-	a_pMeshMngr->AddSphereToQueue(glm::translate(m_pBoundingSphere->GetCenterGlobal()) *   glm::scale(vector3(m_pBoundingSphere->GetRadius()* 2.0f)), m_v3Color, WIRE);
+    a_pMeshMngr->AddCubeToQueue(glm::translate(m_pBoundingBox->GetCenterGlobal()) * glm::extractMatrixRotation(m_m4ToWorld) * glm::scale(m_pBoundingBox->GetHalfWidth() * 2.0f), m_v3Color, WIRE);
+    a_pMeshMngr->AddCubeToQueue(glm::translate(m_pReorientedBoundingBox->GetCenterGlobal()) * glm::scale(m_pReorientedBoundingBox->GetHalfWidth() * 2.0f), m_v3Color, WIRE);
+    a_pMeshMngr->AddSphereToQueue(glm::translate(m_pBoundingSphere->GetCenterGlobal()) *   glm::scale(vector3(m_pBoundingSphere->GetRadius()* 2.0f)), m_v3Color, WIRE);
 }
 
 #pragma region Accessors
+
 void MyBoundingObjectClass::SetModelMatrix(matrix4 a_m4ToWorld)
 {
-	m_m4ToWorld = a_m4ToWorld;
-	
-	m_pBoundingBox->SetModelMatrix(a_m4ToWorld);
-	*m_pReorientedBoundingBox = m_pBoundingBox->GetReorientedBoundingBox();
-	m_pBoundingSphere->SetModelMatrix(a_m4ToWorld);
+    m_m4ToWorld = a_m4ToWorld;
+    
+    m_pBoundingBox->SetModelMatrix(a_m4ToWorld);
+    *m_pReorientedBoundingBox = m_pBoundingBox->GetReorientedBoundingBox();
+    m_pBoundingSphere->SetModelMatrix(a_m4ToWorld);
 }
 
 matrix4 MyBoundingObjectClass::GetModelMatrix() const
 {
-	return m_m4ToWorld;
+    return m_m4ToWorld;
 }
 
 vector3 MyBoundingObjectClass::GetCenterLocal() const
 {
-	return m_v3Center;
+    return m_v3Center;
 }
 
 vector3 MyBoundingObjectClass::GetCenterGlobal() const
 {
-	return vector3(m_m4ToWorld * vector4(m_v3Center, 1.0f));
+    return vector3(m_m4ToWorld * vector4(m_v3Center, 1.0f));
 }
 
 vector3 MyBoundingObjectClass::GetMinimum() const
 {
-	return m_pReorientedBoundingBox->GetMinimum();
+    return m_pReorientedBoundingBox->GetMinimum();
 }
 
 vector3 MyBoundingObjectClass::GetMaximum() const
 {
-	return m_pReorientedBoundingBox->GetMaximum();
+    return m_pReorientedBoundingBox->GetMaximum();
 }
 
-void MyBoundingObjectClass::SetVisibility(boolean a_bVisibility)
+void MyBoundingObjectClass::SetVisibility(bool a_bVisibility)
 {
-	m_bVisibility = a_bVisibility;
+    m_bVisibility = a_bVisibility;
 }
 
-boolean MyBoundingObjectClass::GetVisibility(void)
+bool MyBoundingObjectClass::GetVisibility() const
 {
-	return m_bVisibility;
+    return m_bVisibility;
 }
+
 #pragma endregion
--- Source/MyBoundingObjectClass.h
+++ Source/MyBoundingObjectClass.h
@@ -6,56 +6,58 @@
 #include "MyBoundingBoxClass.h"
 #include "MyBoundingSphereClass.h"
 
+#include <memory> // for std::shared_ptr
+
 class MyBoundingObjectClass
 {
-	MyBoundingBoxClass* m_pBoundingBox;
-	MyBoundingBoxClass* m_pReorientedBoundingBox;
-	MyBoundingSphereClass* m_pBoundingSphere;
+    std::shared_ptr<MyBoundingBoxClass> m_pBoundingBox;
+    std::shared_ptr<MyBoundingBoxClass> m_pReorientedBoundingBox;
+    std::shared_ptr<MyBoundingSphereClass> m_pBoundingSphere;
 
-	boolean m_bVisibility;	// Holds the visibility of the BO
-	vector3 m_v3Color = REBLACK;	// Holds the color of the bounding object
+    bool m_bVisibility;	// Holds the visibility of the BO
+    vector3 m_v3Color = REBLACK;	// Holds the color of the bounding object
 
-	matrix4 m_m4ToWorld = IDENTITY_M4; // Matrix that takes us from local to world coordinates
-	vector3 m_v3Center = vector3(0.0f); // Stores the center point of the Bounding Object
-	vector3 m_v3Min = vector3(0.0f); // Stores the minimum vector of the bounding object Class
-	vector3 m_v3Max = vector3(0.0f); // Stores the maximum vector of the bounding object Class
+    matrix4 m_m4ToWorld = IDENTITY_M4; // Matrix that takes us from local to world coordinates
+    vector3 m_v3Center = vector3(0.0f); // Stores the center point of the Bounding Object
+    vector3 m_v3Min = vector3(0.0f); // Stores the minimum vector of the bounding object Class
+    vector3 m_v3Max = vector3(0.0f); // Stores the maximum vector of the bounding object Class
 
 public:
-	// Creates Bounding Object from list of vertices
-	MyBoundingObjectClass(const std::vector<vector3>& vertices);
+    // Creates Bounding Object from list of vertices
+    MyBoundingObjectClass(const std::vector<vector3>& vertices);
 
-	MyBoundingObjectClass(MyBoundingObjectClass const& other);
+    MyBoundingObjectClass(MyBoundingObjectClass const& other);
 
-	MyBoundingObjectClass& operator=(MyBoundingObjectClass const& other);
+    MyBoundingObjectClass& operator=(MyBoundingObjectClass const& other);
 
-	~MyBoundingObjectClass(void);
-
-
-	void SetVisibility(boolean a_bVisibility);
-
-	boolean GetVisibility(void);
+    ~MyBoundingObjectClass();
 
 
-	vector3 GetCenterLocal(void) const;
+    void SetVisibility(bool a_bVisibility);
 
-	vector3 GetCenterGlobal(void) const;
-
-
-	vector3 GetMinimum(void) const;
-
-	vector3 GetMaximum(void) const;
+    bool GetVisibility() const;
 
 
-	void SetModelMatrix(matrix4 a_m4ToWorld);
-	matrix4 GetModelMatrix(void) const;
+    vector3 GetCenterLocal() const;
 
-	// Sets the color of the Bounding Object
-	void SetColor(vector3 a_v3Color);
+    vector3 GetCenterGlobal() const;
 
-	// Adds the Bounding Object to the Render List
-	void Draw(MeshManagerSingleton*);
 
-	bool IsColliding(MyBoundingObjectClass* const a_pOther) const;
+    vector3 GetMinimum() const;
+
+    vector3 GetMaximum() const;
+
+
+    void SetModelMatrix(matrix4 a_m4ToWorld);
+    matrix4 GetModelMatrix() const;
+
+    // Sets the color of the Bounding Object
+    void SetColor(vector3 a_v3Color);
+
+    // Adds the Bounding Object to the Render List
+    void Draw(MeshManagerSingleton*);
+
+    bool IsColliding(MyBoundingObjectClass* const a_pOther) const;
 };
 
 #endif //__MYBOUNDINGOBJECTCLASS_H_
